94f7f7041d3e4ec8a5c21ff608fdff324ec7913b
jadedevon02 <121012067+jadedevon02@users.noreply.github.com>
2023-02-01 20:57:16 +0800

    Add files via upload

---
+++ preAssesmentActivity1/CRUD/yearNow.java
@@ -0,0 +1,32 @@
+import java.text.Format;
+import java.time.*;
+
+import javax.swing.text.html.FormView;
+public class yearNow{
+    public static void main(String[] args) {
+       Year thisYear = Year.now();
+       String genCode = String.format(format: "%04d", )
+
+       //CODE FOR THE MAIN JAVA
+       for(User  list : coll  ){
+        System.out.println("------------------------------------------");
+        System.out.println("Code : " + list.code);
+        System.out.println("Component Name : " + list.peri);
+        System.out.println("Peripheral Type : " + list.periType);
+        System.out.println("Name : "+ list.name);
+        System.out.println("------------------------------------------");
+
+
+    }
+    if(list.getName()!String.equals("NONE")){
+        System.out.println("-------------------------------");
+        System.out.println("---------NOT AVAILABLE------------");
+        System.out.println("Not Available Peripheral are: "+list.getPeri());
+        System.out.println("Peripheral Type : "+list.getperiType());
+        System.out.println("-------------------------------");
+
+    
+
+}
+    }
+}
